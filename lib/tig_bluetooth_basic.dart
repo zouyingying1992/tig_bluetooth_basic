dfcb647a02fb85351e1770a8616bf7524e0c0902
Nian <i19850511@gmail.com>
2021-05-14 16:51:43 +0800

    first commit

---
+++ lib/tig_bluetooth_basic.dart
@@ -0,0 +1,9 @@
+// You have generated a new plugin project without
+// specifying the `--platforms` flag. A plugin project supports no platforms is generated.
+// To add platforms, run `flutter create -t plugin --platforms <platforms> .` under the same
+// directory. You can also find a detailed instruction on how to add platforms in the `pubspec.yaml` at https://flutter.dev/docs/development/packages-and-plugins/developing-packages#plugin-platforms.
+
+library tig_bluetooth_basic;
+
+export './src/bluetooth_manager.dart';
+export './src/bluetooth_device.dart';
---
+++ test/tig_bluetooth_basic_test.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/services.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:tig_bluetooth_basic/tig_bluetooth_basic.dart';
+
+void main() {
+  const MethodChannel channel = MethodChannel('flutter_bluetooth_basic');
+
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  setUp(() {
+    channel.setMockMethodCallHandler((MethodCall methodCall) async {
+      return '42';
+    });
+  });
+
+  tearDown(() {
+    channel.setMockMethodCallHandler(null);
+  });
+}
